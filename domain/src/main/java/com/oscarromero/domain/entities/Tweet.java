9da6cd9d0e3a85c3e56e596bc50ab3e310cb5488
Oscar Romero <oscar.romero.garreta@gmail.com>
2016-04-30 17:53:18 +0200

    Created a new entity

--- domain/src/main/java/com/oscarromero/domain/entities/Tweet.java
+++ domain/src/main/java/com/oscarromero/domain/entities/Tweet.java
@@ -1,4 +1,42 @@
 package com.oscarromero.domain.entities;
 
+import java.util.Date;
+
 public class Tweet {
+    private User user;
+    private Date createAt;
+    private String message;
+    private int retweets;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getRetweets() {
+        return retweets;
+    }
+
+    public void setRetweets(int retweets) {
+        this.retweets = retweets;
+    }
 }
